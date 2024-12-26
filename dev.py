10d9e68bdfa9766bf5c2a3f369ff3e6e8346fc21
Kvazar-213452 <kvazar382@gmail.com>
2024-12-26 13:25:35 +0200

    linux fix

---
+++ dev.py
@@ -0,0 +1,23 @@
+import subprocess
+
+let = input("Type: ")
+
+# ⣾⡇⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣄⢻⣦⡀⠁⢸⡌⠻⣿⣿⣿⡽⣿⣿
+# ⡇⣿⠹⣿⡇⡟⠛⣉⠁⠉⠉⠻⡿⣿⣿⣿⣿⣿⣦⣄⡉⠂⠈⠙⢿⣿⣝⣿
+# ⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮
+# ⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀
+# ⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸
+# ⠄⠄⢿⣿⡀⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡾⠁⢠⡇⢀
+# ⠄⠄⢸⣿⡇⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣫⣻⡟⢀⠄⣿⣷⣾
+# ⠄⠄⢸⣿⡇⠄⠈⠙⠿⣿⣿⣿⣮⣿⣿⣿⣿⣿⣿⣿⡿⢠⠊⢀⡇⣿⣿
+# ⠒⠤⠄⣿⡇⢀⡲⠄⠄⠈⠙⠻⢿⣿⣿⠿⠿⠟⠛⠋⠁⣰⠇⠄⢸⣿⣿⣿
+
+if let == "0":
+    # Команди для Git
+    name = input("Name: ")
+    subprocess.run(["git", "add", "-A"])
+    subprocess.run(["git", "commit", "-m", name])
+    subprocess.run(["git", "push"])
+elif let == "1":
+    subprocess.run(["sass", "app_front_end/static/prefab/scss/main.scss", "app_front_end/static/css/main.css"])
+    subprocess.run(["sass", "app_front_end/static/prefab/scss/global.scss", "app_front_end/static/css/global.css"])
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { app, BrowserWindow } from 'electron';
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webviewTag: true,  // Дозволяє використання webview
+    },
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});

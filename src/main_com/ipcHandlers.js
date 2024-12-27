30149555f28344c94f961c299eca03069f528bf7
Kvazar-213452 <kvazar382@gmail.com>
2024-12-27 23:12:16 +0200

    end app

--- src/main_com/ipcHandlers.js
+++ src/main_com/ipcHandlers.js
@@ -3,26 +3,28 @@ const fs = require('fs');
 const path = require('path');
 
 const history_file = path.join(__dirname, '../data/history.json');
+const config_file = path.join(__dirname, '../data/config.json');
 
 export const initializeIpcHandlers = () => {
   ipcMain.on('history', async (event, message) => {
     fs.readFile(history_file, 'utf8', (err, data) => {
       if (err) {
-        event.reply('message-response', 'error');
+        event.reply('history_response', 'error');
         return;
       }
       
       try {
         const jsonData = JSON.parse(data);
-        event.reply('message-response', jsonData);
+        event.reply('history_response', jsonData);
       } catch (parseError) {
-        event.reply('message-response', 'error');
+        event.reply('history_response', 'error');
       }
     });
   });
 
   ipcMain.on('history_add', async (event, message) => { 
       let data = [];
+
       if (fs.existsSync(history_file)) {
           const fileContent = fs.readFileSync(history_file, 'utf-8');
           data = JSON.parse(fileContent);
@@ -35,6 +37,7 @@ export const initializeIpcHandlers = () => {
 
   ipcMain.on('history_del', async (event, message) => { 
     let data = [];
+
     if (fs.existsSync(history_file)) {
         const fileContent = fs.readFileSync(history_file, 'utf-8');
         data = JSON.parse(fileContent);
@@ -43,5 +46,14 @@ export const initializeIpcHandlers = () => {
     data = [];
 
     fs.writeFileSync(history_file, JSON.stringify(data, null, 2));
+  });
+
+  ipcMain.on('settings_get', async (event, message) => { 
+    let data = [];
+
+    const fileContent = fs.readFileSync(config_file, 'utf-8');
+    data = JSON.parse(fileContent);
+
+    event.reply('settings_get_response', data);
   });
 };
